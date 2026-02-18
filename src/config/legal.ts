47bd1474f3245dd0be551ef9a99df811a1c9fda9
Nicolau <mateus.n.gomes.f@gmail.com>
2026-02-18 16:59:38 -0300

    feat: add legal pages for privacy policy and terms of use with versioning configuration.

--- src/config/legal.ts
+++ src/config/legal.ts
@@ -1,4 +1,4 @@
-export const CURRENT_TERMS_VERSION = "1.0.0";
+export const CURRENT_TERMS_VERSION = "1.1.0";
 export const LAST_TERMS_UPDATED_DATE = "18 de Fevereiro de 2026";
-export const CURRENT_PRIVACY_VERSION = "1.0.0";
+export const CURRENT_PRIVACY_VERSION = "1.1.0";
 export const LAST_PRIVACY_UPDATED_DATE = "18 de Fevereiro de 2026";
