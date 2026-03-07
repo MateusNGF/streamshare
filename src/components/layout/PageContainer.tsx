c1f96484edba7b4ab9a7fd5cc3f8e740fd2e5da9
Nicolau <mateus.n.gomes.f@gmail.com>
2026-03-07 11:02:38 -0300

    fix: small fix

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -3,10 +3,10 @@ import { ReactNode } from "react";
 
 export function PageContainer({ children }: { children: React.ReactNode }) {
     return (
-        <main className="flex-1 overflow-y-auto h-[calc(100vh-64px)] pb-20  md:p-6 bg-gray-50/50">
+        <div className="flex-1 pb-32 md:p-30 bg-gray-50/50">
             <div className="max-w-7xl mx-auto">
                 {children}
             </div>
-        </main>
+        </div>
     );
 }
